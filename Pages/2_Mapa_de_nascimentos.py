947fb9ac029c2b7ac783f529aff84a2ec17e3b08
Erik Henrique Dias Ferreira <150151321+Erik-Henrique@users.noreply.github.com>
2024-05-22 14:37:39 -0300

    Update 2_Mapa_de_nascimentos.py

--- Pages/2_Mapa_de_nascimentos.py
+++ Pages/2_Mapa_de_nascimentos.py
@@ -5,10 +5,8 @@ import matplotlib.pyplot as plt
 import datetime
 
 def app():
-  st.set_page_config(layout='wide',
-                     page_title='SINASC RONDÔNIA',
-                     page_icon='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Bras%C3%A3o_de_Rond%C3%B4nia.svg/1200px-Bras%C3%A3o_de_Rond%C3%B4nia.svg.png')
-  
+#
+
   page_bg_img = """
     <style>
     [data-testid="stAppViewContainer"] {
