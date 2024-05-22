0f7a17fb0d2e6c791ac2983da1d13cddc80bce14
Erik Henrique Dias Ferreira <150151321+Erik-Henrique@users.noreply.github.com>
2024-05-22 14:36:33 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import streamlit as st
+
 st.set_page_config(layout='wide',
                    page_title='SINASC RONDÔNIA',
                    page_icon='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Bras%C3%A3o_de_Rond%C3%B4nia.svg/1200px-Bras%C3%A3o_de_Rond%C3%B4nia.svg.png')
