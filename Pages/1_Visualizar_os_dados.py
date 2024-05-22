020bea91d2008112dcfaa79398647e991926d355
Erik Henrique Dias Ferreira <150151321+Erik-Henrique@users.noreply.github.com>
2024-05-22 13:49:44 -0300

    Update 1_Visualizar_os_dados.py

--- Pages/1_Visualizar_os_dados.py
+++ Pages/1_Visualizar_os_dados.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(layout='wide',
+                   page_title='SINASC RONDÔNIA',
+                   page_icon='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Bras%C3%A3o_de_Rond%C3%B4nia.svg/1200px-Bras%C3%A3o_de_Rond%C3%B4nia.svg.png')
 def app():
-  st.set_page_config(layout='wide',
-                     page_title='SINASC RONDÔNIA',
-                     page_icon='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Bras%C3%A3o_de_Rond%C3%B4nia.svg/1200px-Bras%C3%A3o_de_Rond%C3%B4nia.svg.png')
-  
   page_bg_img = """
     <style>
     [data-testid="stAppViewContainer"] {
